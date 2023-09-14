fedfcbcf2956e4c0aa03b02d80d9db4d9fc80f86
Kenny Baron <kennybaron@fastmail.com>
2023-09-14 14:23:33 -0400

    fin scanner

--- jlox/src/com/craftinginterpreters/lox/Scanner.java
+++ jlox/src/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,10 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Scanner {
@@ -41,7 +44,8 @@ class Scanner {
 			case '-': addToken(MINUS); break;
 			case '+': addToken(PLUS); break;
 			case ';': addToken(SEMICOLON); break;
-			case '*': addToken(STAR); break;
+			case '*': 
+				addToken(STAR); break;
 			case '!':
 				addToken(match('=') ? BANG_EQUAL : BANG);
 				break;
@@ -57,6 +61,20 @@ class Scanner {
 			case '/':
 				if (match('/')) {
 					while (peek() != '\n' && !isAtEnd()) advance();
+				} else if (match('*')) {
+					while (true) {
+						if (isAtEnd()) {
+							Lox.error(line, "Unterminated block comment.");
+							break;
+						} else if (peek() == '\n') {
+							line++;
+						} else if (peek() == '*' && peekNext() == '/') {
+							advance();
+							advance();
+							break;
+						}
+						advance();
+					}
 				} else {
 					addToken(SLASH);
 				}
@@ -76,10 +94,11 @@ class Scanner {
 			default:
 				if (isDigit(c)) {
 					number();
+				} else if (isAlpha(c)) {
+					identifier();
 				} else {
 					Lox.error(line, "Unexpected character.");
 				}
-				Lox.error(line, "Unexpected character.");
 				break;
 		}
 	}
@@ -149,6 +168,46 @@ class Scanner {
 	private char peekNext() {
 		if (current + 1 >= source.length()) return '\0';
 		return source.charAt(current + 1);
+	}
+
+	private void identifier() {
+		while (isAlphaNumeric(peek())) advance();
+
+		String text = source.substring(start, current);
+		TokenType type = keywords.get(text);
+		if (type == null) type = IDENTIFIER;
+		addToken(type);
+	}
+
+	private boolean isAlpha(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+	}
+
+	private boolean isAlphaNumeric(char c) {
+		return isAlpha(c) || isDigit(c);
+	}
+
+	private static final Map<String, TokenType> keywords;
+
+	static {
+		keywords = new HashMap<>();
+		keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+
 	}
 
 }
