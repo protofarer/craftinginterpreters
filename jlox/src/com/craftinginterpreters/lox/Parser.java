7322f656e15423d858eab4131051f39d0155601c
Kenny Baron <kennybaron@fastmail.com>
2023-09-21 22:36:24 -0400

    fin control flow

--- jlox/src/com/craftinginterpreters/lox/Parser.java
+++ jlox/src/com/craftinginterpreters/lox/Parser.java
@@ -1,6 +1,7 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -194,17 +195,60 @@ public class Parser {
 	}
 
 	private Stmt statement() {
+		if (match(FOR)) return forStatement();
 		if (match(IF)) return ifStatement();
+		if (match(WHILE)) return whileStatement();
 		if (match(PRINT)) return printStatement();
 		if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
 		return expressionStatement();
 	}
 
+	private Stmt forStatement() {
+		consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+		Stmt initializer;
+		if (match(SEMICOLON)) {
+			initializer = null;
+		} else if (match(VAR)) {
+			initializer = varDeclaration();
+		} else {
+			initializer = expressionStatement();
+		}
+
+		Expr condition = null;
+		if (!check(SEMICOLON)) {
+			condition = expression();
+		}
+
+		consume(SEMICOLON, "Expect ';' after loop condition.");
+
+		Expr increment = null;
+		if (!check(SEMICOLON)) {
+			increment = expression();
+		}
+		consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+
+		Stmt body = statement();
+
+		if (increment != null) {
+			body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+		}
+
+		if (condition == null) condition = new Expr.Literal(true);
+		body = new Stmt.While(condition, body);
+
+		if (initializer != null) {
+			body = new Stmt.Block(Arrays.asList(initializer, body));
+		}
+
+		return body;
+	}
+
 	private Stmt ifStatement() {
 		consume(LEFT_PAREN, "Expect '(' after 'if'.");
 		Expr condition = expression();
-		consume(RIGHT_PAREN, "Expect ')' after if condition.");
+		consume(RIGHT_PAREN, "Expect ')' after 'if' condition.");
 
 		Stmt thenBranch = statement();
 		Stmt elseBranch = null;
@@ -231,6 +275,16 @@ public class Parser {
 
 		consume(SEMICOLON, "Expect ';' after variable declaration.");
 		return new Stmt.Var(name, initializer);
+	}
+
+	private Stmt whileStatement() {
+		consume(LEFT_PAREN, "Expected '(' after 'while'.");
+		Expr condition = expression();
+		consume(RIGHT_PAREN, "Expected ')' after 'while' condition.");
+
+		Stmt body = statement();
+
+		return new Stmt.While(condition, body);
 	}
 
 	// expressions that have side effects, eg function/ or method call followed by ';'
